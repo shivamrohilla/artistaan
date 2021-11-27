15070e1c3cd2eb6d3d75c996e87a51555467e44e
Aman kamboj <kamboj.aman32@gmail.com>
2021-11-28 00:52:22 +0530

    USP-section  added

---
+++ src/app/usp/usp.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    
+    selector: 'app-usp',
+    templateUrl: 'usp.component.html',
+    styleUrls: ['usp.component.css']
+})
+
+export class UspComponent {
+}
